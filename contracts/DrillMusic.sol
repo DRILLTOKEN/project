d3d4a6343e387c4e30a522e6a11ce09bd3f7b741
Remix Workspace <>
2022-08-08 21:26:29 +0300

    contracts

--- contracts/DrillMusic.sol
+++ contracts/DrillMusic.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.4;
 
 interface IERC20 {
@@ -405,7 +406,7 @@ contract DRILL is Context, IERC20, Ownable {
     }
     constructor () {
         _rOwned[owner()] = _rTotal;
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0xbe24F871CC2C46057251f19dA32d8366aDE6e624);
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
         uniswapV2Router = _uniswapV2Router;
